c04b35d41a567f44a7a1d5ca20ba216a3eebe9b7
Taisia <alleanoraanges@gmail.com>
2024-02-27 17:50:08 +0300

    add preload sceen

--- src/sceen/prelode.ts
+++ src/sceen/prelode.ts
@@ -14,6 +14,29 @@ class PreloadGame extends Phaser.Scene {
         super("PreloadGame");
     }
     preload() {
+        const progressBar = this.add.graphics();
+        const progressBox = this.add.graphics();
+        
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
+
+        const { width } = this.cameras.main;
+        const { height } = this.cameras.main;
+        const loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                color: '#ffffff'
+            }
+        })
+        loadingText.setOrigin(0.5, 0.5)
+        this.load.on('progress', (value: number) => {
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        })
         this.load.tilemapTiledJSON('map', map)
         this.load.image('waterTile', water)
         this.load.image('grassTile', grass)
@@ -42,9 +65,12 @@ class PreloadGame extends Phaser.Scene {
             frameWidth: 50,
             frameHeight: 53
         })
-    }
-    create() {
-        this.scene.start('Game')
+        this.load.on('complete', () => {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            this.scene.start('Game')
+        })
     }
 }
 
